a75a39fd7490013eda09865a8ef1fbf61e9e369f
Daniel O'Connor <clockwerx@php.net>
2009-08-11 00:50:30 +0000

    Added stub unit tests
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_Safe/trunk@287066 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/HTML_SafeTest.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'PHPUnit/Framework/TestCase.php';
+
+class HTML_SafeTest extends PHPUnit_Framework_TestCase {
+    public function testShouldParseCorrectly() {
+        $this->markTestIncomplete("Implement test coverage of parse() under a number of scenarios");
+        /*
+        function HTML_Safe() 
+        function _closeHandler(&$parser, $name) 
+        function _closeTag($tag) 
+        function _dataHandler(&$parser, $data) 
+        function _escapeHandler(&$parser, $data) 
+        function _openHandler(&$parser, $name, $attrs) 
+        function _writeAttrs ($attrs) 
+        function clear() 
+        function getXHTML () 
+        function parse($doc) 
+        function repackUTF7($str)
+        function repackUTF7Back($str)
+        function repackUTF7Callback($str)
+        */
+    }
+}
