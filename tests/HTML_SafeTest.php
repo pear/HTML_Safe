85ef8f7cde43d22a715aa877d5a1452e071e6c69
Giorgio Scalvini <giorgio.scalvini@exainnovation.com>
2023-07-27 18:25:38 +0200

    PSR-2 compliancy - Convert line separator from CRLF to LF and fix typos

--- tests/HTML_SafeTest.php
+++ tests/HTML_SafeTest.php
@@ -8,12 +8,12 @@ class HTML_SafeTest extends TestCase
 {
     public function testAllowTags()
     {
-        $input    = '<html><body><p>my text</p></body></html>'; 
+        $input    = '<html><body><p>my text</p></body></html>';
         $expected = '<body><p>my text</p></body>';
 
         $safe = new HTML_Safe;
         $safe->setAllowTags(array('body'));
-        $this->assertSame($expected, $safe->parse($input)); 
+        $this->assertSame($expected, $safe->parse($input));
     }
 
     public function testSpecialChars()
