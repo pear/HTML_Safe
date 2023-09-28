33f8e84e701b30e33fe11d2cb7727b8d7b5c2a77
Giorgio Scalvini <g.scalvini98@gmail.com>
2023-09-28 08:45:19 +0200

    Add tests for missing closure tag, missing open tag, paragraph closure, single tag and insecure tag

--- tests/HTML_SafeTest.php
+++ tests/HTML_SafeTest.php
@@ -34,4 +34,53 @@ class HTML_SafeTest extends TestCase
         $this->assertSame($expectedOne, $safe->parse($inputOne));
         $this->assertSame($expectedTwo, $safe->parse($inputTwo));
     }
+
+    public function testMissingClosureTag()
+    {
+        $input = '<div><span>my text with missing tag closure</div>';
+        $expected = '<div><span>my text with missing tag closure</span></div>';
+
+        $safe = new HTML_Safe();
+        $this->assertSame($expected, $safe->parse($input));
+    }
+
+    public function testMissingOpenTag()
+    {
+        $input = '<div>my text with missing tag opening</span></div>';
+        $expected = '<div>my text with missing tag opening</div>';
+
+        $safe = new HTML_Safe();
+        $this->assertSame($expected, $safe->parse($input));
+    }
+
+    public function testParagraphClosure()
+    {
+        $input = '<div><h1>my first title</h1><p>my text<h1>my second title</h1></p></div>';
+        $expected = '<div><h1>my first title</h1><p>my text</p><h1>my second title</h1></div>';
+
+        $safe = new HTML_Safe();
+        $this->assertSame($expected, $safe->parse($input));
+    }
+
+    public function testSingleTags()
+    {
+        $inputOne = '<img src="not-exist.jpg" alt="Test image">';
+        $expectedOne = '<img src="not-exist.jpg" alt="Test image" />';
+
+        $inputTwo = '<img src="not-exist.jpg" alt="Test image">TEST</img>';
+        $expectedTwo = '<img src="not-exist.jpg" alt="Test image" />TEST';
+
+        $safe = new HTML_Safe();
+        $this->assertSame($expectedOne, $safe->parse($inputOne));
+        $this->assertSame($expectedTwo, $safe->parse($inputTwo));
+    }
+
+    public function testInsecureTags()
+    {
+        $input = '<div>Iframe content:<iframe title="Github iframe" width="300" height="200" src="https://github.com"></iframe></div>';
+        $expected = '<div>Iframe content:</div>';
+
+        $safe = new HTML_Safe();
+        $this->assertSame($expected, $safe->parse($input));
+    }
 }
